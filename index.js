6937d4e3c00297801113522b13f770540ba7156b
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-10 23:21:06 -0600

    para recivir a varios jugadores dentro del juego, se utiliza un array y luego, se le pide a un cliente servidor, algo

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express")
 
 const app = express()
+const jugadores = []
 
-app.get("/", (req,res)=>{
+app.get("/unirse", (req,res)=>{ //nuestra pagina en el fronted llame un servicio en el backend para que se regriste ese jugador y le devuelva un id
     res.send("Hellooo")
-}) //dar un recurso. para eso es get
+
+}) 
+
+
 
 app.listen(8080, ()=>{
     console.log("Servidor funcionando");
 })
+
+//http, localhost, servidores y puertos
+
