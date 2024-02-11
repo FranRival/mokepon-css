aad9a6b99a7db78852b0b14cf20e5d7e01720a25
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-10 23:09:28 -0600

    creamos una palicacion con express js, y la url raiz, responda un hola. y que escuche continuemente en el puerto 8080, las peticiones de los clientes para que todo el tiempo pueda responderles

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Esto es node y un mensaje simple');
+const express = require("express")
+
+const app = express()
+
+app.get("/", (req,res)=>{
+    res.send("Hellooo")
+}) //dar un recurso. para eso es get
+
+app.listen(8080, ()=>{
+    console.log("Servidor funcionando");
+})
