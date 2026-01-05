6427cde9046e6ce21660adbc5449d46260037356
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2026-01-04 19:31:36 -0600

    prioridad al proyecto de la flota de webs

--- js/javascript.js
+++ js/javascript.js
@@ -340,3 +340,5 @@ let ataqueEnemigoActual = ataqueEnemigo[ataqueEnemigo.length - 1]
 
 //reestructurando secienciaAtaque
 //el tipo actual (codigo agrgado a delete7) no interviene en el codigo real. el problema, es que estamos usando el emoji como identificador de ataque. cuando debemos usar el tipo.
+
+//le dimos prioridad a la flota de webs. 
