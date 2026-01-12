d6f1555804721d12eb33610cba78e0b66b5d23ca
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2026-01-11 21:51:34 -0600

    corrigiendo errores.

--- js/javascript.js
+++ js/javascript.js
@@ -345,4 +345,4 @@ let ataqueEnemigoActual = ataqueEnemigo[ataqueEnemigo.length - 1]
 //creo que me estoy volviendo flojo. 
 //otro dia para delegar this tarea
 //other day... ya debo de comenzar a ponerme las pilas alv
-//comenzar a hacer mi linkedin
+//comenzar a hacer mi linkedin. tenemos que volver a programar. 
